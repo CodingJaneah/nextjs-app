ca6f8795d128af4b24899ef2a3f29a15ac642da3
Janeah <marianajaneramos05@gmail.com>
2025-09-26 17:12:02 +0800

    update

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -14,7 +14,7 @@ const page = async () => {
   return (
     <div>
       <h1>Users</h1>
-      <p>{new Date().toString()}</p>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user =>
         <li key={user.id}>
